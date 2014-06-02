81aa12824599c160e684c484f28b159eed846385
Evin Ugur <evinugur96@gmail.com>
2014-06-01 22:40:20 -0400

    cleanup

--- server.js
+++ server.js
@@ -18,35 +18,44 @@ var person = function() {
 	};
 }
 
+// Texas <td> elements index via cheerio, might have to update if Texas updates their site
+var INDEX_COUNTY = 19;
+var INDEX_RACE = 17;
+var INDEX_DATE = 15;
+var INDEX_AGE = 13;
+var INDEX_TDCJ = 11;
+var INDEX_FIRST_NAME = 9;
+var IDNEX_LAST_NAME = 7;
+var INDEX_EXECUTION_NUMBER = 1;
+
+var people = [];
 
 app.get('/scrape', function(req, res){
   var url = "http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html";
   request(url, function(error, response, html){
         if(!error){
-            
-
-
             var $ = cheerio.load(html);
             var people = [];
             var entries = $("tr");
             for (var i = 1; i < entries.length; i++) {
-            	var listing = entries[i];
+            	var children = entries[i].children;
             	var current_person = person();
-            	current_person.county = listing.children[19].children[0].data;
-            	current_person.race = listing.children[17].children[0].data;
-            	current_person.date = listing.children[15].children[0].data;
-            	current_person.age = listing.children[13].children[0].data;
-            	current_person.TDCJ_number = listing.children[11].children[0].data;
-            	current_person.firt_name = listing.children[9].children[0].data;
-            	current_person.last_name = listing.children[7].children[0].data;
-            	current_person.execution_number = listing.children[1].children[0].data;
-            	
-            	console.log(current_person);
-            	break;
+            	current_person.county = children[INDEX_COUNTY].children[0].data;
+            	current_person.race = children[INDEX_RACE].children[0].data;
+            	current_person.date = children[INDEX_DATE].children[0].data;
+            	current_person.age = children[INDEX_AGE].children[0].data;
+            	current_person.TDCJ_number = children[INDEX_TDCJ].children[0].data;
+            	current_person.firt_name = children[INDEX_FIRST_NAME].children[0].data;
+            	current_person.last_name = children[IDNEX_LAST_NAME].children[0].data;
+            	current_person.execution_number = children[INDEX_EXECUTION_NUMBER].children[0].data;
+                console.log(current_person);
+                people.push(current_person);
+                break;
             }
+
 		}
 	});	
 });
 app.listen('8081');
-console.log('Magic happens on port 8081');
+console.log('talk to localhost::8081/scrape to generate data');
 exports = module.exports = app;
